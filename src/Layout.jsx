b940dcdead7beed3692cdfc592e001b8e4f0c2a1
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-04-22 21:48:21 +0100

    adding pages to it

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,4 @@
-import Header from "./header/Header";
+import Header from "./component/header/Header";
 import Footer from "./component/Footer/Footer";
 import { Outlet } from "react-router-dom";
 import React from 'react'

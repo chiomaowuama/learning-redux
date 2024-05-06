3e428204ff03c0b078af84349c5089f05355d20c
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-05-06 12:24:02 +0100

    moving to another ep

--- reactpart1/src/component/Home/Home.jsx
+++ reactpart1/src/component/Home/Home.jsx
@@ -1,21 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Home() {
   const [advice, setAdvice] = useState()
+  const[count, setCount] = useState(0)
   async function getApi(){
     const resp = await fetch('https://api.adviceslip.com/advice');
     const res = await resp.json()
     setAdvice(res.slip.advice)
     console.log(res.slip.advice)
+    setCount((c) => c + 1)
   }
 
+  useEffect(function(){
+    getApi()
+  },[])
   return (
     <>
     <div>
       <h1>{advice}</h1>
       <button onClick={getApi}>Get advice</button>
+      <Message count={count - 1}/>
+      {/* <p> you have read <strong>{ count }</strong> pieces of advice</p> */}
+
     </div>
     </>
+  )
+}
+function Message(props){
+  return(
+    <p> you have read <strong>{ props.count }</strong> pieces of advice</p>
+
   )
 }
 

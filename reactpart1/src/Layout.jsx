6390d96c195139918c4bfc63eb8a74e5e02ee5a3
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-05-06 09:56:38 +0100

    first react app

--- reactpart1/src/Layout.jsx
+++ reactpart1/src/Layout.jsx
@@ -1,2 +1,16 @@
-import Footer from "./Component/Footer/Footer";
+import React from 'react'
+import Footer from "./component/Header/Header";
 import Header from "./component/Header/Header";
+import {Outlet} from 'react-router-dom'
+
+function Layout() {
+  return (
+    <>
+    <Header />
+    <Outlet />
+    <Footer />
+    </>
+  )
+}
+
+export default Layout
